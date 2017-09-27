809627c6d3278e5d32c486387ea3c9aa557351eb
sherzo <saulflorez.backend@gmail.com>
2017-09-27 11:37:30 -0430

    Mixex

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import config from './config'
 
 const { apiKey } = config
-const URL = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${apiKey}&format=json`
+const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${apiKey}&format=json`
 
 export default function getArtists(country) {
 	const url = URL.replace(':country', country)
