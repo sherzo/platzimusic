9fcadd5acda3edc099dd6a02172e3a671528b45f
sherzo <saulflorez.backend@gmail.com>
2017-09-27 09:53:14 -0430

    Estructurando componentes

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@
     img(src='./assets/logo.png')
     h1 Platzimusic
     ul
-      li(v-for="artist in artists") {{ artist.name }}
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
 </template>
 
 <script>
+import Artist from './components/Artist.vue'
 import getArtists from './api'
 
 export default {
@@ -15,6 +16,9 @@ export default {
     return {
       artists: []
     }
+  },
+  components: {
+    Artist
   },
   mounted: function () {
     const self = this
