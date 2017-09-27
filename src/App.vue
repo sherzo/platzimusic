439f032925a915e556084e9ea195e060e00f236e
sherzo <saulflorez.backend@gmail.com>
2017-09-27 10:26:08 -0430

    Actualizando datos en la interfaz. Top artistas por pais

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   #app
     img(src='./assets/logo.png')
     h1 Platzimusic
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
     ul
       artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
 </template>
@@ -14,18 +16,34 @@ export default {
   name: 'app',
   data () {
     return {
-      artists: []
+      artists: [],
+      countries: [
+        {name: 'Argentina', value: 'argentina'},
+        {name: 'Colombia', value: 'colombia'},
+        {name: 'España', value: 'spain'}
+      ],
+      selectedCountry: 'argentina'
     }
   },
   components: {
     Artist
   },
-  mounted: function () {
-    const self = this
-    getArtists()
+  methods: {
+    refreshArtist() {
+      const self = this
+      getArtists(this.selectedCountry)
       .then(function (artists) {
         self.artists = artists;
       });
+    }
+  },
+  mounted() {
+    this.refreshArtist()
+  },
+  watch: {
+    selectedCountry(){
+      this.refreshArtist()
+    }
   }
 }
 </script>
